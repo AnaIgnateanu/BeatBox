6414b02d265b07c248f3244909581dc682bea358
Ana Ignateanu <ana.m.ignateanu@gmail.com>
2017-04-25 15:06:36 +0300

    first commit

---
+++ app/src/main/java/com/example/ana/beatbox/Picture.java
@@ -0,0 +1,77 @@
+package com.example.ana.beatbox;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by ana on 4/25/2017.
+ */
+
+public class Picture {
+    public Drawable getRoundedCornerImage(Drawable bitmapDrawable) {
+        Bitmap bitmap = ((BitmapDrawable)bitmapDrawable).getBitmap();
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 10;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        Drawable image = new BitmapDrawable(output);
+        return image;
+
+    }
+
+    public static Drawable getImage(Context context, String name) {
+        return context.getResources().getDrawable(context.getResources().getIdentifier(name, "drawable", context.getPackageName()));
+    }
+
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+// Read in the dimensions of the image on disk
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+// Figure out how much to scale down by
+        int inSampleSize = 3;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            if (srcWidth > srcHeight) {
+                inSampleSize = Math.round(srcHeight / destHeight);
+            } else {
+                inSampleSize = Math.round(srcWidth / destWidth);
+            }
+        }
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+// Read in and create final bitmap
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay()
+                .getSize(size);
+        return getScaledBitmap(path, size.x, size.y);
+    }
+}
